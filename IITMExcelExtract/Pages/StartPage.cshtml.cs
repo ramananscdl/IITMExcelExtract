a77f571b44b511c871cfffa1af492b7091c83d2f
ramananscdl <ramananscdl@gmail.com>
2023-02-21 08:16:54 +0530

    Add project files.

---
+++ IITMExcelExtract/Pages/StartPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IITMExcelExtract.Pages
+{
+    public class StartPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
